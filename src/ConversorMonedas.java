7675811d7fd045da6a86c121aeee7c4b9e5d3947
rogergvr <rogervr@gmail.com>
2025-06-01 17:23:30 -0700

    Optimizando Codigo

--- src/ConversorMonedas.java
+++ src/ConversorMonedas.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ConversorMonedas {
     public void desplegarMenuOpciones(){
         String menuConversorMonedas = """
@@ -22,5 +24,43 @@ public class ConversorMonedas {
                 """;
 
         System.out.println(menuConversorMonedas);
+    }
+
+    public String otraMoneda (int opcionIngresada){
+        switch (opcionIngresada){
+            case 0, 1:
+                return "ARS";
+            case 2, 3:
+                return "BRL";
+            case 4, 5:
+                return "COP";
+            case 6, 7:
+                return "CLP";
+            case 8, 9:
+                return "PEN";
+        }
+        return "";
+    }
+
+    public void mostrarResultado(String monedaOrigen,
+                                 String monedaConversion,
+                                 Double montoOrigen){
+
+        ConsultaMoneda consulta = new ConsultaMoneda();
+        Moneda moneda = consulta.buscaExchangeRate(monedaOrigen, monedaConversion);
+
+        System.out.printf("El monto de %s %.2f es equivalente a %s %.2f \n\n", monedaOrigen, montoOrigen, monedaConversion, montoOrigen * Double.valueOf(moneda.conversion_rate()));
+
+        return;
+    }
+
+    public boolean deseaContinuar(Scanner teclado){
+        System.out.println("Desea continuar (S/N): ");
+        if (teclado.nextLine().equalsIgnoreCase("N")) {
+            System.out.println("Gracias por utilizar nuestro Conversor de Monedas!");
+            return false;
+        }
+        else
+            return true;
     }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -3,85 +3,48 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int opcionIngresada = 0;
         String opcion = "";
+        int opcionIngresada = 0;
         String monedaOrigen = "";
         String monedaConversion = "";
         double montoOrigen = 0;
-        double montoConvertido = 0;
-        ConsultaMoneda consulta = new ConsultaMoneda();
         ConversorMonedas conversor = new ConversorMonedas();
 
         while (opcionIngresada <= 9){
             conversor.desplegarMenuOpciones();
             opcion = teclado.nextLine();
-            if (opcion.equalsIgnoreCase("X"))
-                opcionIngresada = 10;
-            else {
-                opcionIngresada = Integer.parseInt(opcion);// valueOf(opcion);
-                //System.out.println("Opcion ingresada: " + opcionIngresada);
-            }
 
-            switch (opcionIngresada) {
-                case 0:
-                    monedaOrigen = "USD";
-                    monedaConversion = "ARS";
-                    break;
-                case 1:
-                    monedaOrigen = "ARS";
-                    monedaConversion = "USD";
-                    break;
-                case 2:
-                    monedaOrigen = "USD";
-                    monedaConversion = "BRL";
-                    break;
-                case 3:
-                    monedaOrigen = "BRL";
-                    monedaConversion = "USD";
-                    break;
-                case 4:
-                    monedaOrigen = "USD";
-                    monedaConversion = "COP";
-                    break;
-                case 5:
-                    monedaOrigen = "COP";
-                    monedaConversion = "USD";
-                    break;
-                case 6:
-                    monedaOrigen = "USD";
-                    monedaConversion = "CLP";
-                    break;
-                case 7:
-                    monedaOrigen = "CLP";
-                    monedaConversion = "USD";
-                    break;
-                case 8:
-                    monedaOrigen = "USD";
-                    monedaConversion = "PEN";
-                    break;
-                case 9:
-                    monedaOrigen = "PEN";
-                    monedaConversion = "USD";
-                    break;
-                case 10:
-                    System.out.println("Gracias por utilizar nuestro Conversor de Monedas!");
+            try {
+                if (opcion.equalsIgnoreCase("X"))
+                    opcionIngresada = 10;
+                else {
+                    opcionIngresada = Integer.parseInt(opcion);// valueOf(opcion);
+                }
+
+                switch (opcionIngresada){
+                    case 0, 2, 4, 6, 8:
+                        monedaOrigen = "USD";
+                        monedaConversion = conversor.otraMoneda(opcionIngresada);
+                        break;
+                    case 1, 3, 5, 7, 9:
+                        monedaOrigen = conversor.otraMoneda(opcionIngresada);
+                        monedaConversion = "USD";
+                        break;
+                    case 10:
+                        System.out.println("Gracias por utilizar nuestro Conversor de Monedas!");
+                        return;
+                    default:
+                        //System.out.println("No ha ingresado una opción válida");
+                        throw new NumberFormatException();
+                }
+
+                System.out.printf("Ingrese el monto en %s que desea convertir: ", monedaOrigen);
+                conversor.mostrarResultado(monedaOrigen, monedaConversion, Double.valueOf(teclado.nextLine()));
+                if (!conversor.deseaContinuar(teclado))
                     return;
-                default:
-                    System.out.println("No ha ingresado una opción válida");
             }
-            //System.out.printf("Monedas seleccionadas: %s y %s", monedaOrigen, monedaConversion);
-
-            Moneda moneda = consulta.buscaExchangeRate(monedaOrigen, monedaConversion);
-            //System.out.println(moneda);
-            System.out.println("Ingrese el monto que desea convertir");
-            montoOrigen = Double.valueOf(teclado.nextLine());
-            montoConvertido = montoOrigen * Double.valueOf(moneda.conversion_rate());
-            System.out.printf("El monto de %s %.2f es equivalente a %s %.2f \n\n", monedaOrigen, montoOrigen, monedaConversion, montoConvertido);
-            System.out.println("Desea continuar (S/N): ");
-            opcion = teclado.nextLine();
-            if (opcion.equalsIgnoreCase("N")){
-                System.out.println("Gracias por utilizar nuestro Conversor de Monedas!");
-                return;
+            catch (NumberFormatException e){
+                System.out.println("No ha ingresado una opción válida. Por favor, revise las opciones.\n\n");
             }
         }
     }
